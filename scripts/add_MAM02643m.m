c94cfa3ccce28db48fdcb0fa4c53e3e60628bdcc
CKTJames <177745254+CKTJames@users.noreply.github.com>
2025-10-27 11:03:29 +0000

    script fix: updated add_MAM02643.m to include the metabolites.tsv edits

--- scripts/add_MAM02643m.m
+++ scripts/add_MAM02643m.m
@@ -12,3 +12,24 @@ metsToAdd.metCharges = -1
 ihuman1 = addMets(ihuman, metsToAdd)
 
 metabolites = importTsvFile("Human-GEM/model/metabolites.tsv")
+
+idx = 4516;
+
+fillfields = {'mets', 'metsNoComp', 'metKEGGID', 'metChEBIID'}
+
+fields = fieldnames(metabolites)
+
+for f = 1:numel(fields)
+thisField = fields{f};
+
+% Insert an empty cell placeholder for all fields
+metabolites.(thisField)(idx+1:end+1) = metabolites.(thisField)(idx:end);
+metabolites.(thisField){idx} = [];
+end
+
+metabolites.mets{idx} = 'MAM02643m'
+metabolites.metsNoComp{idx} = 'MAM02643'
+metabolites.metKEGGID{idx} = 'C05752';
+metabolites.metChEBIID{idx} = 'MNXM979';
+
+exportTsvFile(metabolites, 'metabolites_mod.tsv')
